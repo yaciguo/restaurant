6809ccd584d0a9ca5dc7b70771abbca806bb12fa
DESKTOP-M42M6QO\Acer <r12435687@gmail.com>
2023-06-22 13:52:35 +0800

    import dishBean CascadeType

--- springprojEEIT64/src/main/java/com/ispan/eeit64/entity/DishBean.java
+++ springprojEEIT64/src/main/java/com/ispan/eeit64/entity/DishBean.java
@@ -3,6 +3,7 @@ package com.ispan.eeit64.entity;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
