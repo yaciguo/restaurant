d4e9f251fba2d92ed7915759a7e1f9386631de5f
fireous <zzzzzx12@gmail.com>
2023-07-07 16:43:36 +0800

    fix API class

--- springprojEEIT64/src/main/java/com/ispan/eeit64/controller/APIResult/APIResult.java
+++ springprojEEIT64/src/main/java/com/ispan/eeit64/controller/APIResult/APIResult.java
@@ -7,24 +7,6 @@ import lombok.Data;
 @Data
 public class APIResult {
     private Object data;
-    public Object getData() {
-		return data;
-	}
-	public void setData(Object data) {
-		this.data = data;
-	}
-	public Integer getCode() {
-		return code;
-	}
-	public void setCode(Integer code) {
-		this.code = code;
-	}
-	public String getMsg() {
-		return msg;
-	}
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
 	private Integer code;
     private String msg;
 
